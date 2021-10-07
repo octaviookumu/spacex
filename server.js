b2d3e442011478780cdafa78019f9708dee40b94
Octavian Okumu <octaviookumu@gmail.com>
2021-10-07 10:56:49 +0300

    Setting up query on client side

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema')
+const cors = require('cors')
 
 const app = express();
+
+// Allow cross origin
 
 app.use(
   "/graphql",
